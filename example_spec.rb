35cd394a4543461ce34f0a2bd028079dc99d8d3a
satzz <satzz21@gmail.com>
2017-04-10 00:31:51 +0900

    pass instance to method

--- example_spec.rb
+++ example_spec.rb
@@ -1,30 +1,46 @@
 class User
-  def self.create(_)
+  def initialize(*_)
     # ...
   end
 
-  def rename(_)
+  def update_avatar(_)
     # ...
   end
+end
+
+class Avatar
+  def initialize(path)
+    @path = path
+  end
+
+  attr_reader :path
 end
 
 describe User do
   it 'receives class method call' do
     expect(described_class)
-      .to receive(:create) do |name|
+      .to receive(:new).with('Bill')
+
+    described_class.new('Bill')
+  end
+
+  it 'receives class method call' do
+    expect(described_class)
+      .to receive(:new) do |name|
         expect(name).to eq 'Bill'
       end
 
-    described_class.create('Bill')
+    described_class.new('Bill')
   end
 
   it 'receives instance method call' do
     expect_any_instance_of(described_class)
-      .to receive(:rename) do |instance, name|
-        expect(instance).to be_an_instance_of described_class
-        expect(name).to eq 'Tom'
+      .to receive(:update_avatar) do |user, avatar|
+        expect(user).to be_an_instance_of described_class
+        expect(avatar).to be_an_instance_of Avatar
+        expect(avatar.path).to eq './avatar.png'
       end
 
-    described_class.new.rename('Tom')
+    described_class.new.update_avatar(Avatar.new('./avatar.png'))
   end
 end
